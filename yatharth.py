bf62842049a19e7050626299c0d6ff2537ba221c
yatharth0404 <pandeyyatharth50@gmail.com>
2025-08-02 12:14:53 +0530

    Create yatharth.py
    Addition, Subtraction, Multiplication, Division
    of two numbers 
    first number = 5
    second number = 10

---
+++ yatharth.py
@@ -0,0 +1,37 @@
+
+#Addition of two numbers
+a = int (input ("enter the first number:  "))
+b = int (input ("enter the second number:  "))
+sum = a+b
+print (sum)
+
+
+
+
+
+#Substraction of two numbers
+a = int (input ("enter the first number:  "))
+b = int (input ("enter the second number:  "))
+diff = a-b
+print (diff)
+
+
+
+
+
+#Multiplication of two number
+a = int(input("enter the first number:  "))
+b = int(input("enter the second number:  "))
+mult = a*b
+print(mult)
+
+
+
+
+
+
+#Division of two number
+a = int(input("enter the first number:  "))
+b = int(input("enter the second number:"))
+result = a/b
+print (result)
